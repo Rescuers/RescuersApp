5474c34d653f027d70d9c1320cf40d5fb80660a6
vgrandhi <venkatesh.grandhi@hexagonsi.com>
2019-10-18 14:57:08 +0530

    pushed the changes

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -20,6 +20,10 @@ import {
   Alert
 } from 'react-native';
 import * as fabfirebaseapp from 'react-native-firebase';
+import Geocoder from 'react-native-geocoder';
+import { getCurrentLocation, getLocationFromPosition } from './GeocodeService';
+import { isGoogleMapsInstalled, PermissionsHelper } from './PermissionHelper';
+import { showLocation } from 'react-native-map-link';
 
 export interface LandingPageProps {
   result?: ResultModel
@@ -39,6 +43,49 @@ export interface ResultModel {
   imageUrl: any
 }
 
+/**
+ * Model of position object.
+ */
+export interface GeoPosition {
+  coords: {
+    latitude: number;
+    longitude: number;
+    altitude: number | null;
+    accuracy: number | null;
+    altitudeAccuracy: number | null;
+    heading: number | null;
+    speed: number | null;
+  };
+  timestamp: number;
+};
+
+/**
+ * contains success status and address from the native platform geocode service * 
+ */
+export interface GeoServiceResponse {
+  isSuccess: boolean,
+  address?: GeoLocationAddress,
+  error: any
+}
+
+/**
+ * Model for returned address from native platform geocode service
+ */
+export interface GeoLocationAddress {
+  position: { lat: number, lng: number },
+  formattedAddress: string
+  feature: string | null
+  streetNumber: string | null,
+  streetName: string | null,
+  postalCode: string | null,
+  locality: string | null,
+  country: string,
+  countryCode: string,
+  adminArea: string | null
+  subAdminArea: string | null,
+  subLocality: string | null
+}
+
 
 export default class LandingPage extends Component<LandingPageProps, LandingPageState> {
   constructor(props: LandingPageProps, state: LandingPageState) {
@@ -48,32 +95,28 @@ export default class LandingPage extends Component<LandingPageProps, LandingPage
     }
   }
 
-  onNotificationReceive = (data: any) => {
+  onNotificationReceive = async (data: any) => {
 
-    const ref = fabfirebaseapp.storage().ref(data.imageUrl);
-    ref.getDownloadURL().then(_imageURL => {
+    let locationText = await this.fetchLocationText(data);
+    let imageUrl = await this.fetchImageUrl(data);
 
-      let result: ResultModel = {
-        id: Math.floor((Math.random() * 1000) + 1),
-        location: "123",
-        category: 'Floods',
-        objectType: "Person",
-        time: new Date().toLocaleString(),
-        status: "Open",
-        imageUrl: _imageURL
-      }
-  
-      console.log('result', result)
-  
-      if (result.location) {
-        let results = this.state.results;
-        results.push(result)
-        this.setState({ results: results ? results : [] })
-      }
-    }).catch(error => {
-      console.log('error', error)
+    let result: ResultModel = {
+      id: Math.floor((Math.random() * 1000) + 1),
+      location: locationText,
+      category: 'Floods',
+      objectType: "Person",
+      time: new Date().toLocaleString(),
+      status: "Open",
+      imageUrl: imageUrl
+    }
 
-    })
+    console.log('result', result)
+
+    if (result.location) {
+      let results = this.state.results;
+      results.push(result)
+      this.setState({ results: results ? results : [] })
+    }
 
   }
 
@@ -98,6 +141,80 @@ export default class LandingPage extends Component<LandingPageProps, LandingPage
     );
   }
 
+
+
+
+  onRoute = async (item: ResultModel) => {
+
+    const permission = await PermissionsHelper.requestPermission("location", "Enable Location Services", "Please go to settings and enable location services");
+    if (!permission) {
+      Alert.alert("No internet", 'Please check your network connection and try again.'
+        , [{ text: 'OK', onPress: () => { } }], { cancelable: false });
+      return;
+    }
+
+    let values: string[] = item.location.split(',');
+    getCurrentLocation().then((response: any) => {
+      if (response.isSuccess) {
+        console.log('response.address', response.address)
+        const { lat, lng } = response.address.position;
+        isGoogleMapsInstalled().then((isInstalled) => {
+          if (isInstalled) {
+            showLocation({
+              latitude: values[0],
+              longitude: values[1],
+              sourceLatitude: lat,
+              sourceLongitude: lng,
+              app: 'google-maps'
+            })
+          }
+          else {
+            Alert.alert('Route', 'Unable to route, Please try again.', [{ text: 'OK', onPress: () => { } }], { cancelable: false })
+          }
+
+        })
+      } else {
+        Alert.alert('Route', "Failed to fetch current location, Please try again.", [{
+          text: 'OK', onPress: () => {
+          }
+        }], { cancelable: false })
+      }
+    }).catch((error: any) => { });
+  }
+
+  private async fetchImageUrl(data: any) {
+    let imageUrl = undefined
+    if (!data || !data.imageUrl) {
+      return imageUrl
+    }
+    try {
+      let ref = fabfirebaseapp.storage().ref(data.imageUrl);
+      imageUrl = await ref.getDownloadURL();
+    }
+    catch (error) {
+    }
+    return imageUrl;
+  }
+
+  private async fetchLocationText(data: any) {
+    let locationText = '';
+    let values: string[] = data.location.split(',');
+    if (!values || values.length < 2) {
+      return locationText
+    }
+    let geoServiceResponse = await getLocationFromPosition({
+      coords: {
+        latitude: parseInt(values[0]),
+        longitude: parseInt(values[1])
+      }
+    });
+    if (geoServiceResponse.isSuccess) {
+      locationText = geoServiceResponse.address.formattedAddress;
+    }
+    console.log('address', geoServiceResponse);
+    return locationText;
+  }
+
   render() {
     return (
       <>
@@ -105,50 +222,63 @@ export default class LandingPage extends Component<LandingPageProps, LandingPage
         <SafeAreaView style={styles.container}>
 
           <View style={{ backgroundColor: '#2F95D6', borderRadius: 7 }}>
-            <Text style={{ fontSize: 25, color: 'white', alignSelf: 'center' }}>{'Rescuers'}</Text>
+            <TouchableOpacity onPress={() => {
+              // this.onNotificationReceive({
+              //   imageUrl: "f08f2747-ad3f-4b24-a316-0a2a7410858b.jpg",
+              //   location: "17.402963,78.376705"
+              // } as ResultModel)
+            }}>
+              <Text style={{ fontSize: 25, color: 'white', alignSelf: 'center' }}>{'Rescuers'}</Text>
+            </TouchableOpacity>
           </View>
           {
-            this.state.results.length > 0 ?  
-            <FlatList
-            data={this.state.results}
-            renderItem={({ item }) => {
-              console.log('imageURL', item.imageUrl);
+            this.state.results.length > 0 ?
+              <FlatList
+                data={this.state.results}
+                renderItem={({ item }) => {
+                  console.log('imageURL', item.imageUrl);
 
-              return (<View style={styles.item}>
-                <View style={{ flexDirection: 'row', paddingVertical: 3 }}>
-                  <Image style={{ width: 20, height: 20, resizeMode: 'contain' }} source={require('../images/icon_location.png')} />
-                  <Text style={styles.textStyle}>{item.location}</Text>
-                </View>
-                <View style={{ flexDirection: 'row', paddingVertical: 3 }}>
-                  <Image style={{ width: 20, height: 20, resizeMode: 'contain' }} source={require('../images/icon_time.png')} />
-                  <Text style={styles.textStyle}>{item.time}</Text>
-                </View>
-                <View style={{ flexDirection: 'row', paddingVertical: 3 }}>
+                  return (<View style={styles.item}>
+                    <View style={{ flexDirection: 'row', paddingVertical: 3, alignItems: 'center', paddingRight: 5 }}>
+                      <View style={{ flexDirection: 'row' }}>
+                        <TouchableOpacity onPress={() => this.onRoute(item)} style={{ flexDirection: 'row' }}>
+                          <Image style={{ width: 20, height: 20, resizeMode: 'contain', alignSelf: 'center' }} source={require('../images/icon_location.png')} />
+                          <Text style={styles.textStyle}>{item.location}</Text>
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                    <View style={{ flexDirection: 'row', paddingVertical: 3 }}>
+                      <Image style={{ width: 20, height: 20, resizeMode: 'contain' }} source={require('../images/icon_time.png')} />
+                      <Text style={styles.textStyle}>{item.time}</Text>
+                    </View>
+                    {/* <View style={{ flexDirection: 'row', paddingVertical: 3 }}>
                   <Image style={{ width: 20, height: 20, resizeMode: 'contain' }} source={require('../images/icon_objectType.png')} />
                   <Text style={styles.textStyle}>{item.objectType}</Text>
-                </View>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingVertical: 3, paddingRight: 5 }}>
-                  <View style={{ flexDirection: 'row'}}>
-                    <View style={[styles.statusIndicator, { backgroundColor: item.status == 'closed' ? 'green' : 'yellow' }]}></View>
-                    <Text style={[styles.textStyle, { marginHorizontal: 15 }]}>{item.status}</Text>
-                  </View>
-                  <TouchableOpacity onPress={() => this.onPress(item)} style={{ alignSelf: 'center'}}>
-                    <View>
-                      <Image style={{ width: 25, height: 25, resizeMode: 'cover', }}
-                        source={require('../images/icon_delete.png')} />
+                </View> */}
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingVertical: 3, paddingRight: 5 }}>
+                      <View style={{ flexDirection: 'row' }}>
+                        <View style={[styles.statusIndicator, { backgroundColor: item.status == 'closed' ? 'green' : 'yellow' }]}></View>
+                        <Text style={[styles.textStyle, { marginHorizontal: 15 }]}>{item.status}</Text>
+                      </View>
+                      <TouchableOpacity onPress={() => this.onPress(item)} style={{ alignSelf: 'center' }}>
+                        <View>
+                          <Image style={{ width: 20, height: 20, resizeMode: 'cover', }}
+                            source={require('../images/icon_delete.png')} />
+                        </View>
+                      </TouchableOpacity>
                     </View>
-                  </TouchableOpacity>
-                </View>
-                <Image style={{ width: '100%', height: 150 }} resizeMode='stretch' 
-                  source={{uri: item.imageUrl }} />
-              </View>)
+                    <Image style={{ width: '100%', height: 200 }} resizeMode='stretch'
+                      source={{ uri: item.imageUrl }} />
+                  </View>)
 
-            }}
-            keyExtractor={item => item.id.toString()}
-          /> :
-          <Text style={{fontSize: 25}}>{'No rescues tasks assigned. You will receive notification and please stay tuned'}</Text>
+                }}
+                keyExtractor={item => item.id.toString()}
+              /> :
+              <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+                <Text style={{ fontSize: 25, textAlignVertical: 'center', textAlign: 'center' }}>{'No rescues tasks assigned. You will receive notification and please stay tuned'}</Text>
+              </View>
           }
-          
+
         </SafeAreaView>
       </>
     );
@@ -199,5 +329,5 @@ const styles = StyleSheet.create({
     width: 20,
     backgroundColor: 'rgb(255,255,255)',
     marginTop: 4,
-}
+  }
 });
